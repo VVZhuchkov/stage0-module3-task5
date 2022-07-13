f6348043c62af74e5b0c2fd7d507f253a42de9f5
VVZhuchkov <vzhuchkov@tut.by>
2022-07-13 17:31:25 +0300

    stage0-module3-task5 Done

--- src/main/java/lang/print/gaps/task5/TimeConvertor.java
+++ src/main/java/lang/print/gaps/task5/TimeConvertor.java
@@ -2,5 +2,8 @@ package lang.print.gaps.task5;
 
 public class TimeConvertor {
     public void convert(float minutes) {
+        final float SECONDS_IN_MINUTE = 60f;
+        float seconds = minutes * SECONDS_IN_MINUTE;
+        System.out.println(seconds);
     }
 }
